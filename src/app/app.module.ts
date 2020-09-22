00cb8a040e5501181e459671048e9725afc18f54
Isabelly Dias Faustino <isabellydias29@gmail.com>
2020-09-22 11:52:44 -0300

    Create header and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
